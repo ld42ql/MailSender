10c7214095220d1f233d8fe190bc7b2487d68f81
Vladimir baleev <ld42ql@hotmail.com>
2018-05-14 17:43:21 +0300

    Unit-test по классу SchedulerClass

---
+++ CodePasswordDLL.Tests/CodePasswordTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PasswordDLL;
+
+namespace CodePasswordDLL.Tests
+{
+    [TestClass]
+    public class CodePasswordTests
+    {
+        [TestMethod]
+        public void getCodPassword_abc_bcd()
+        {
+            // arrange
+            string strIn = "abc";
+            string strExpected = "bcd";
+
+            // act
+            string strActual = Cryptographer.GetCodPassword(strIn);
+
+            //assert
+            Assert.AreEqual(strExpected, strActual);
+        }
+    }
+}
